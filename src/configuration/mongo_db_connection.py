df756bc76753c7263b4a6d7f6d02957009c1b8a3
MD-IESA-ALI-AHMED <mdiesa.24@kgpian.iitkgp.ac.in>
2026-06-26 07:40:32 +0530

    all experiments done

--- src/configuration/mongo_db_connection.py
+++ src/configuration/mongo_db_connection.py
@@ -47,6 +47,8 @@ class MongoDBClient:
             # Check if a MongoDB client connection has already been established; if not, create a new one
             if MongoDBClient.client is None:
                 mongo_db_url = os.getenv(MONGODB_URL_KEY)  # Retrieve MongoDB URL from environment variables
+                print("ENV URL:", mongo_db_url)
+                print("Database:", database_name)
                 if mongo_db_url is None:
                     raise Exception(f"Environment variable '{MONGODB_URL_KEY}' is not set.")
                 
